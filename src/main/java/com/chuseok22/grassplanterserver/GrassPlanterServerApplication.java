4209d6e857ccddc3d13c02b9544f424b36a563be
Chuseok22 <bjh59629@naver.com>
2025-12-24 17:15:49 +0900

    project init

---
+++ src/main/java/com/chuseok22/grassplanterserver/GrassPlanterServerApplication.java
@@ -0,0 +1,13 @@
+package com.chuseok22.grassplanterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrassPlanterServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GrassPlanterServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/chuseok22/grassplanterserver/GrassPlanterServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuseok22.grassplanterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrassPlanterServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
